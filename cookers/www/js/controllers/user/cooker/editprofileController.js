3b2100fb6c61d6ac3a2fd0fbdbfa458995589fe3
ksw1652 <ksw1652@gmail.com>
2015-10-29 19:11:42 +0900

    10.29 2차 성우 커밋

--- cookers/www/js/controllers/user/cooker/editprofileController.js
+++ cookers/www/js/controllers/user/cooker/editprofileController.js
@@ -25,9 +25,9 @@ angular.module('cookers.controllers')
                 change_pw : '',
                 change_pw_check : '',
                 change_pw_current : '',
-                change_pw_current_status : false,
+                change_pw_current_status : true,
                 imgdata : '',
-                change_pw_status : false,
+                change_pw_status : true,
                 change_pw_msg : '비밀번호가 일치하지 않아요',
                 photo : ''
             };
@@ -42,30 +42,17 @@ angular.module('cookers.controllers')
             };
 
             $scope.completeProfileEvent = function(){
-                if($scope.profileedit.change_pw == '' && $scope.profileedit.change_pw_status == false){
-                    /**
-                     * 암것도 안함 pw 지정해줄 필요 없음
-                     */
-                    alert('pw변경안함');
-                }else{
-                    /**
-                     * pw 저장
-                     */
-                    alert('pw변경');
-                }
 
                 if($scope.profileedit.name == ''){
                     /**
                      * 이름 변경 안함, 원래것 너어줌
                      */
                     $scope.profileedit.name = $scope.cooker_profile.name;
-                    alert('이름변경안함');
 
                 }else{
                     /**
                      * 이름 변경함
                      */
-                    alert('이름변경');
                 }
 
                 if($scope.profileedit.state_comment == ''){
@@ -73,29 +60,19 @@ angular.module('cookers.controllers')
                      * 상태글 변경 안함, 원래것 너어줌
                      */
                     $scope.profileedit.state_comment = $scope.cooker_profile.state_comment;
-                    alert('상태글 변경 안함');
                 }else{
                     /**
                      * 상태글 변경
                      */
-                    alert('상태글 변경');
                 }
-                if($scope.profile_image_send == ''){
-                    /**
-                     * 사진 변경 안함, 원래것 너어줌
-                     */
-                    $scope.profileedit.photo = $scope.cooker_profile.cooker_photo;
-                    alert('사진 변경안함');
+
+                if($scope.profileedit.imgdata == 'img/chef31.png'){
+                    $scope.profileedit.photo = '';
                 }else{
-                    /**
-                     * 사진 변경
-                     */
                     $scope.profileedit.photo = $localStorage.id + '.png';
-                    alert('사진 변경');
-
                 }
 
-                if($scope.profileedit.change_pw_status == false ){
+                if($scope.profileedit.change_pw_status == false || $scope.profileedit.change_pw_current_status == false){
                     $ionicLoading.show({
                         template: '<i class="ion-android-cancel"></i> 비밀번호 입력을 확인해주세요'
                         , noBackdrop: true
@@ -103,20 +80,24 @@ angular.module('cookers.controllers')
                     });
                 }else{
                     if($scope.profile_image_send != ''){
-
                         /**
                          * 돌리기
                          */
 
                         cookerService.uploadProfileImageHttpRequest($localStorage.id, $scope.profile_image_send).then(function(server_data){
+
                             cookerService.editProfileHttpRequest($scope.profileedit, $localStorage.id).then(function(result){
+
                                 $state.go('tabs.user', {userid:$localStorage.id});
+
                             });
+
                         })
                     }else{
                         /**
                          * 그냥 변경만
                          */
+
                         cookerService.editProfileHttpRequest($scope.profileedit, $localStorage.id).then(function(result){
                             $state.go('tabs.user', {userid:$localStorage.id});
                         });
@@ -162,9 +143,19 @@ angular.module('cookers.controllers')
             $scope.pwchangetoggleEvent = function(){
                 if($scope.pwchange == false){
                     $scope.pwchange = true;
+                    $scope.profileedit.change_pw_current_status = false;
+
                 }else{
                     $scope.pwchange = false;
+                    $scope.profileedit.change_pw_current_status = true;
+
                 }
+            };
+
+            $scope.currentPwCheckEvent = function(){
+                cookerService.checkPwHttpRequest($localStorage.id, $scope.profileedit.change_pw_current).then(function(result){
+                    $scope.profileedit.change_pw_current_status = result;
+                })
             };
 
             $rootScope.$on('changepwstateChange', function (event, data) {
@@ -187,10 +178,6 @@ angular.module('cookers.controllers')
                         }
                     }
                 }
-
             });
-
-
-
         }
     ]);
